17d8e4c58c8e49ff09cf3d5a456d1a74ede6260d
KevinGomez30 <kgomezlo24bj@ikzubirimanteo.com>
2024-11-27 13:29:38 +0100

    Vida y prefabs de enemigos

--- Assets/Scripts/Enemigo.cs
+++ Assets/Scripts/Enemigo.cs
@@ -3,9 +3,14 @@ using UnityEngine;
 public class Enemigo : MonoBehaviour
 {
     [SerializeField] GameObject explosionPrefab;
+    [SerializeField] int vida = 4;
     private void OnParticleCollision(GameObject other)
     {
-        Instantiate(explosionPrefab, this.transform.position, Quaternion.identity);
-        Destroy(this.gameObject);
+        vida--;
+        if (vida <= 0)
+        {
+            Instantiate(explosionPrefab, this.transform.position, Quaternion.identity);
+            Destroy(this.gameObject);
+        }
     }
 }
