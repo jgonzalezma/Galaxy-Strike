f3a8ce19255886b6b2469e767fc7ec7f659dd6ec
KevinGomez30 <kgomezlo24bj@ikzubirimanteo.com>
2024-11-27 12:22:56 +0100

    Ajuste de escena

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -5,7 +5,6 @@ public class CollisionHandler : MonoBehaviour
     [SerializeField] GameObject explosionPrefab;
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("Collided with: " + other.name);
         Instantiate(explosionPrefab, this.transform.position, Quaternion.identity);
         Destroy(this.gameObject);
     }
